c5ff50a75b17bd7ecb8b8decf6fcf661d4510578
jaeyonging <wodyd1318@naver.com>
2025-11-20 17:41:33 +0900

    trying to fix retry react-query when error happens

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -2,63 +2,91 @@ import axios from "axios";
 import { NEXON_API, NEXON_API_KEY } from "../util/server";
 
 export const fetchSA_UserId = async (nickname: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/id`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            user_name: nickname
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/id`, {
+            headers: {
+                'x-nxopen-api-key': `${NEXON_API_KEY}`
+            },
+            params: {
+                user_name: nickname
+            }
+        });
+        console.log('fetchSA_UserId', response.status);
+        return response.data;
+    } catch (err: any) {
+        throw err; 
+    }
 }
 
 export const fetchSA_UserBasicInfo = async (ouid: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/basic`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            ouid: ouid
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/basic`, {
+            headers: {
+                'x-nxopen-api-key': `${NEXON_API_KEY}`
+            },
+            params: {
+                ouid: ouid
+            }
+        });
+        console.log('fetchSA_UserBasicInfo', response.status);
+
+        return response.data;
+    } catch (err: any) {
+        throw err; 
+    }
 }
 
 export const fetchSA_UserRank = async (ouid: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/rank`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            ouid: ouid
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/rank`, {
+            headers: {
+                'x-nxopen-api-key': `${NEXON_API_KEY}`
+            },
+            params: {
+                ouid: ouid
+            }
+        });
+        console.log('fetchSA_UserRank', response.status);
+
+        return response.data;
+    } catch (err: any) {
+        throw err; 
+    }
 }
 
 export const fetchSA_UserTier = async (ouid: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/tier`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            ouid: ouid
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/tier`, {
+            headers: {
+                'x-nxopen-api-key': `${NEXON_API_KEY}`
+            },
+            params: {
+                ouid: ouid
+            }
+        });
+        console.log('fetchSA_UserTier', response.status);
+
+        return response.data;
+    } catch (err: any) {
+        throw err; 
+    }
 }
 
 export const fetchSA_UserRecentInfo = async (ouid: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/recent-info`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            ouid: ouid
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/user/recent-info`, {
+            headers: {
+                'x-nxopen-api-key': NEXON_API_KEY
+            },
+            params: {
+                ouid
+            }
+        });
+        console.log('fetchSA_UserRecentInfo', response.status);
+        return response.data;
+    } catch (err: any) {
+        throw err; 
+    }
 }
 
 
@@ -66,26 +94,39 @@ export const fetchSA_UserRecentInfo = async (ouid: string) => {
 
 
 export const fetchSA_AllMatchInfo = async (ouid: string, matchMode: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/match`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            ouid: ouid,
-            match_mode: matchMode
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/match`, {
+            headers: {
+                'x-nxopen-api-key': NEXON_API_KEY
+            },
+            params: {
+                ouid,
+                match_mode: matchMode
+            }
+        });
+
+        console.log('fetchSA_AllMatchInfo', response.status);
+        return response.data;
+
+    } catch (err: any) {
+        throw err; 
+    }
 }
 
+
 export const fetchSA_MatchInfoDetail = async (matchId: string) => {
-    const response = await axios.get(`${NEXON_API}/suddenattack/v1/match-detail`, {
-        headers: {
-            'x-nxopen-api-key': `${NEXON_API_KEY}`
-        },
-        params: {
-            match_id: matchId
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${NEXON_API}/suddenattack/v1/match-detail`, {
+            headers: {
+                'x-nxopen-api-key': NEXON_API_KEY
+            },
+            params: {
+                match_id: matchId
+            }
+        });
+        console.log('fetchSA_MatchInfoDetail', response.status);
+        return response.data;
+    } catch (err: any) {
+        throw err; 
+    }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,21 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      retry: (failureCount, error: any) => {
+        if (error?.response?.status === 429) {
+          return failureCount < 3; 
+        }
+        if (error?.response?.status >= 400 && error?.response?.status < 500) {
+          return false;
+        }
+        return failureCount < 3;
+      },
+      retryDelay: (attemptIndex, error: any) => {
+        if (error?.response?.status === 429) {
+          return Math.min(1000 * Math.pow(2, attemptIndex), 8000);
+        }
+        return Math.min(1000 * Math.pow(2, attemptIndex), 30000);
+      },
     },
   },
 });
